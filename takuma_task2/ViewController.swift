27d25cdeb28eb87197fb145defe1b4aa932de360
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-06-19 14:09:25 +0900

    feedbackの内容確認

--- takuma_task2/ViewController.swift
+++ takuma_task2/ViewController.swift
@@ -14,11 +14,29 @@ enum ArithmeticOperation {
     case divide
 }
 
+// 失敗可能イニシャライザはguardと組み合わせて使うのが吉
+private extension ArithmeticOperation {
+    init?(selectedSegmentIndex: Int) {
+        switch selectedSegmentIndex {
+        case 0:
+            self = .add
+        case 1:
+            self = .subtract
+        case 2:
+            self = .multiply
+        case 3:
+            self = .divide
+        default:
+            return nil
+        }
+    }
+}
+
 final class ViewController: UIViewController {
     @IBOutlet weak private var firstNumberTextField: UITextField!
     @IBOutlet weak private var secondNumberTextField: UITextField!
     @IBOutlet weak private var answerLabel: UILabel!
-    private var calculation: ArithmeticOperation = .add
+    @IBOutlet weak private var methodSegmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,37 +44,32 @@ final class ViewController: UIViewController {
         secondNumberTextField.keyboardType = .numberPad
     }
 
-    @IBAction private func changeCalculationMethod(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            calculation = .add
-        case 1:
-            calculation = .subtract
-        case 2:
-            calculation = .multiply
-        case 3:
-            calculation = .divide
-        default:
-            calculation = .add
-        }
-    }
-
     @IBAction private func tapCalculateButton(_ sender: UIButton) {
+        let index = methodSegmentedControl.selectedSegmentIndex
+        guard let calculation = ArithmeticOperation(selectedSegmentIndex: index) else {
+            // nilが返ってきたらボタンを押しても何も起こらない
+            return
+        }
+
+        let answerText: String
+
         let num1 = Double(firstNumberTextField.text!) ?? 0.0
         let num2 = Double(secondNumberTextField.text!) ?? 0.0
         switch calculation {
         case .add:
-            answerLabel.text = String(num1 + num2)
+            answerText = String(num1 + num2)
         case .subtract:
-            answerLabel.text = String(num1 - num2)
+            answerText = String(num1 - num2)
         case .multiply:
-            answerLabel.text = String(num1 * num2)
+            answerText = String(num1 * num2)
         case .divide:
             guard num2 != 0.0 else {
-                answerLabel.text = "割る数には0以外を入力してください"
-                return
+                answerText = "割る数には0以外を入力してください"
+                break
             }
-            answerLabel.text = String(num1 / num2)
+            answerText = String(num1 / num2)
         }
+
+        answerLabel.text = answerText
     }
 }
